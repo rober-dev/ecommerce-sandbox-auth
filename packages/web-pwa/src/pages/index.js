9385d82a15402b10063a1cc2881a859b5ed73a5c
rober <me@rober.dev>
2020-09-07 11:24:01 +0200

    Setup Material-UI in next.js

--- packages/web-pwa/src/pages/index.js
+++ packages/web-pwa/src/pages/index.js
@@ -1,12 +1,21 @@
 // Vendor libs
 import React from 'react';
 
+// Material-UI libs
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
 // Component definition
 const HomePage = () => {
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <Container maxWidth='sm'>
+      <Box my={4}>
+        <Typography variant='h4' component='h1' gutterBottom>
+          Next.js example
+        </Typography>
+      </Box>
+    </Container>
   );
 };
 
